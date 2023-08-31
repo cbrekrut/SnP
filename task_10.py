2da7346c1eae4cd6d599265a70d521870b4e0865
Alex <aleksandreremeev75@gmail.com>
2023-08-31 10:21:29 +0300

    Change task10 file

--- task_10.py
+++ task_10.py
@@ -1,22 +1,20 @@
 def count_words(string):
     words = string.lower().split()
     word_count = {}
-
+    
     for word in words:
-        word = word.replace(".,!?", "").replace("-", "")  
+        word = word.strip('.,!?-')
+        
         if word:
             if word in word_count:
                 word_count[word] += 1
             else:
                 word_count[word] = 1
-
+    
     return word_count
 
-result1 = count_words("A man, a plan, a canal -- Panama")
-print(result1)
-# Вывод: {"a": 3, "man": 1, "canal": 1, "panama": 1, "plan": 1}
-
-result2 = count_words("Doo bee doo bee doo")
-print(result2)
-# Вывод: {"doo": 3, "bee": 2}
+# Тесты
+print(count_words("A man, a plan, a canal -- Panama"))  # {'a': 3, 'man': 1, 'plan': 1, 'canal': 1, 'panama': 1}
+print(count_words("Doo bee doo bee doo")) 
+    
 
